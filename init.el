c76cf9c34ed86c8538dbe141acd3ab310646cb90
Erich Ericson <fakefakefans@gmail.com>
2022-08-10 22:32:13 +0200

    init.el: add major cc mode tabbing

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 (tool-bar-mode -1)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+(add-hook 'c-mode-common-hook (lambda () (setq c-basic-offset 4)))
 ;; Customization
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (let ((default-directory "~/.emacs.d/packs"))
